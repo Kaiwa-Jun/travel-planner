052796ba46a45609a9e5f3c0092efe34d128f6fe
Kaiwa-Jun <kj.11235813213455@gmail.com>
2025-02-06 01:07:43 +0900

    buildエラーの修正

--- front/next.config.js
+++ front/next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
